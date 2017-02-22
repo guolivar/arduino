191d03dd0a48de2c8f491ece9f2c2c38dc0fb99d
Ulfasaar <weyers.ryan@gmail.com>
2017-02-23 10:07:23 +1300

    Started writing code for IR sensor

--- Code/Sensor tests/Distance/src/Distance.cpp
+++ Code/Sensor tests/Distance/src/Distance.cpp
@@ -5,11 +5,42 @@ void setup(){
     Serial.begin(9600);
 }
 
-// takes the array finds the lenght
-int normalize(int * nums){
-  return sum / size;
+
+int sum(int * nums, size_t size){
+
+  int result = 0;
+  for (size_t i = 0; i < size; i++) {
+    result += nums[i];
+  }
+
+  return result;
+}
+
+
+int avg(int * nums, size_t repetitions){
+  return sum(nums, repetitions) / repetitions;
+}
+
+// try this to see if it works properly as a map functuib
+// template<typename CONT, typename FUNC>
+// auto map(CONT const& in, FUNC f) -> mapresult<CONT, FUNC>
+// {
+//     return mapresult<CONT, FUNC>(in, f);
+// }
+
+int * collect_distances(unsigned int pin, size_t ammount){
+
+  int collection[ammount];
+
+  for (size_t i = 0; i < ammount; i++) {
+    collection[i] = analogRead(pin);
+  }
+
+  return collection;
 }
 
 void loop() {
-  //Serial.println(analogRead(2));
+
+  unsigned int repetitions = 50;
+  Serial.println( avg( collect_distances(2, repetitions), repetitions ) );
 }
