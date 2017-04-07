79045c0777f04a76233b8a64e4baa9fc7d96dbb9
Ryan Weyers <weyers.ryan@gmail.com>
2017-04-07 22:21:02 +1200

    removed node modules folder and starting to setup tdd for server

--- Code/Server/server.js
+++ Code/Server/server.js
@@ -12,21 +12,53 @@ const PORT=8080;
 // create the connection to database
 var connection = mysql.createConnection({host:'localhost', user: 'root', database: 'skomobo', password: 'dev1234'});
 
-// simple query
-connection.query('SELECT * FROM `sensor_data`', function (err, results, fields) {
-  console.log(results); // results contains rows returned by server
-  console.log(fields); // fields contains extra meta data about results, if available
-});
+function extract(url){
+     // breaks up each value by a dash and removes / in the front
+    var tokens = request.url.slice(1).split('_')
+
+    // layout how the data is going to be mapped
+    // use javascript array.map for this somehow
+    var values = {
+        Dust: tokens[0],
+        Temperature: tokens[1],
+        Humidity: tokens[2],
+        CO2: tokens[3],
+        Decibals: tokens[4],
+        Distance1: tokens[5],
+        Distance2: tokens[6],
+        Distance3: tokens[7],
+        Distance4: tokens[8],
+        Distance5: tokens[9],
+        Distance6: tokens[10],
+        Distance7: tokens[11],
+        Presence: tokens[12] === 'true',
+        Time: tokens[13]
+    }
+
+    return values
+}
 
 function handleRequest(request, response){
-    
+
+    // for browser testing
     if(request.url != '/favicon.ico'){
 
-        var text = request.url.split('-').join(' ').slice(1)        
+       values = extract(request.url)
+
         // io.appendFile('log.txt', text + '\n', (err) =>{
         //     if(err) console.log(err)
         // })
+        console.log(values)
+            // simple query
+        connection.query('INSERT INTO sensor_data SET Dust = ?', values  , function (error, results, fields) {
+        // note fields is a list
+            if(error) throw error;
+            // console.log(error, error.code, error.errno, error.sqlState); // results contains rows returned by server
+        
+        //   console.log(fields); // fields contains extra meta data about results, if available
+        });
 
+        connection.end();
     }
 
 }
