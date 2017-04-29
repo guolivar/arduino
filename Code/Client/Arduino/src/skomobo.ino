d945924678fea88fb191d9a68751fe1840d50a84
Ulfasaar <weyers.ryan@gmail.com>
2017-04-30 02:52:12 +1200

    Got platform io almost working

--- Code/Client/Arduino/src/skomobo.ino
+++ Code/Client/Arduino/src/skomobo.ino
@@ -1,7 +1,7 @@
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
 
 //void print( String text){
 //  Serial.print(text);
