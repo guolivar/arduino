5df27284c1b10db59106177fbd8e9690fe18717e
Ulfasaar <weyers.ryan@gmail.com>
2017-05-01 00:00:54 +1200

    Updated python setup file

--- Code/Client/Raspberry pi/setup.py
+++ Code/Client/Raspberry pi/setup.py
@@ -3,5 +3,7 @@ from distutils.core import setup
 setup(
     name='SKOMOBO',
     version='0.1dev',
-    packages=['spidev', 'coconut']
+    packages=['spidev', 'coconut'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('../../../README.md').read(),
 )
