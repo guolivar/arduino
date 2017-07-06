4652ab8c1999b1ebe6b09ac0c02b1a2b8d3ea3e4
Ulfasaar <weyers.ryan@gmail.com>
2017-07-06 21:51:54 +1200

    Wifi now connects to hotspot!!!!!! and no dependency on wifi esp!!!

--- Main/NewWifi.ino
+++ Main/NewWifi.ino
@@ -4,13 +4,13 @@
 //char ssid[] = "DESKTOP-73HN8ON 5011";           // your network SSID (name)
 //char pass[] = "u65879Q0"; 
 
-
-//int status = WL_IDLE_STATUS;     // the Wifi radio's status
-
 //char server[] = "192.168.100.100";
 //char server[] = "seat-skomobo.massey.ac.nz";
 
 // returns a boolean allowing it to function like a async function if it fails it returns false
+
+bool Connected = false;
+
 boolean send_next(const __FlashStringHelper *command){
 //  Serial.println(Serial.readString());
   // make sure to only continue if OK is received after each command
@@ -21,7 +21,7 @@ boolean send_next(const __FlashStringHelper *command){
   else{
     
 //    Serial.println(Serial.readString());
-  
+    Connected = false;
     return false;
   }
 }
@@ -57,80 +57,75 @@ void pause_print(int num){
   inspect();
 }
 
+
+// refer to this link for what each AT command does 
+//https://www.espressif.com/sites/default/files/documentation/4a-esp8266_at_instruction_set_en.pdf
+
 // setup this macro to tidy up lines
 //#define CMD( arg ) "AT+"arg
+
 void WIFI_setup(){
 
-  // consider setting the timeout to something smaller
-  
-  // see if its there using AT
-  // if it says OK then continue else keep trying 5 times
-    
   Serial.println(F("AT"));
-//  Serial.println(Serial.readString());
+  // Serial.println(Serial.readString());
+  
   if(!Serial.find("OK")){
     try_again(F("AT"), 4);
     Serial.println(F("WIFI module not connected"));
+
+    Connected = false;
   }
   else{
 
-    //disable echoing because it causes the module to crash
-//    Serial.println(F("ATE0"));
-    
-    // when ok send AT+RST to restart it
-    // means that if any settings are still stored then it will start fresh
-  
-    // tidy this up by sending a array with all these commands in it to one function that iterates them and sends next each one
-//    send_next(F("AT+RST"));
+    //restart 
     Serial.println(F("AT+RST"));
-//send_next(F("AT+RST"));
-  
-    // Send ATE0 to stop the esp from echoing back the commands again
-    
+
+    //disable echo
     send_next(F("ATE0"));
-     
-    // Send AT+CIPMUX=1 to set multiple connections to enable multiple connections for server mode
-    // set to 0 by default
+    send_next(F("AT+CWMODE=1"));
+    send_next(F("AT+CIPMUX=1"));
+    send_next(F("AT+CIPDINFO=1"));
+    send_next(F("AT+CWAUTOCONN=0"));
+    send_next(F("AT+CWDHCP=1,1"));
+//    send_next(F("AT+GMR"));
+//    delay(3000);
+    // will send version num not ok
+    //
+//    Serial.println(F("AT+CIPSTATUS"));
+    delay(3000);
+   
+    // set to 0 by default?
 //    send_next(F("AT+CIPMUX=0"));
-
+    
     // set to client mode
-    send_next(F("AT+CWMODE_CUR=1"));
+//    AT+CIPMUX=1
+//AT+CIPDINFO=1
+//AT+CWAUTOCONN=0
+//AT+GMR
+//AT+CIPSTATUS
   
-    // Send AT+CWAUTOCONN=0 to prevent autoconnecting? apparently it causes issues
     // default seems to be 0 but not sure use this to debug if necessary
-//    send_next(F("AT+CWAUTOCONN=0"));
-//    send_next(F("AT+CWAUTOCONN?"));
-    
-    // Send AT+CWHDCP=1,1 to enable DHCP for dynamic IP addresses from the hotspot
+    // send_next(F("AT+CWAUTOCONN=0"));
 
-    // defaults to 3 dont know if this refers to channel 3 or something else weird
-    send_next(F("AT+CWDHCP_CUR=1,1"));
-//    pause_print(1000);
-//    pause_print(2000);
-    // Send AT+GMR to get version number
-//    send_next(F("AT+GMR")); 
-    
-    // Send AT+CWJAP_CUR="ssid""password" to connect to the wifi
+    // defaults to 3 for some reason
+//    send_next(F("AT+CWDHCP=1,1"));
     // TODO make these login details more secure
 
-    send_next(F("AT+CWJAP_CUR=\"4G UFI_8B8\",\"1234567890\""));
-
-    pause_print(1000);
-//    after("OK", inspect);
-//    if(Serial.find("ERROR")){
-      
-//    }
-    //crashes here remove _Cur
-//    Serial.println(Serial.readString());
-
-    //get local IP address
-    send_next(F("AT+CIFSR"));
-
-    Serial.println(Serial.readString());
-  
-    // Send AT+CIPSTATUS to see the ip address etc -- we shouldnt need this but for testing could be useful
-//    
-  
+//    send_next(F("AT+CWJAP=\"4G UFI_8B8\",\"1234567890\""));
+    Serial.println(F("AT+CWJAP_CUR=\"DESKTOP-73HN8ON5011\",\"u65879Q0\""));
+//    send_next(F("AT+CWJAP_CUR=\"DESKTOP-73HN8ON5011\",\"u65879Q0\""));
+    delay(3000);
+//    pause_print(1000);
+//    inspect();
+    if(Serial.find("OK")){
+      Connected = true;
+//      Serial.println(F("AT+CIFSR"));
+//      inspect();
+    }
+    else{
+      Serial.println(F("No AP connection"));
+    }
+ 
     // assign to status variable to say we are ready, if not ready then try 5 times
   }
   
@@ -142,19 +137,14 @@ void WIFI_send(String Time, bool PIR, String Temp, int CO2, String Dust){
   // Send AT+CIPSTART=3, "TCP", "server", servers_http_port
 
   // need to put check in to see if we have connection to hostpot if not retry
-  Serial.println(F("AT+CIPSTART=\"TCP\", \"192.168.100.100\", 81"));
 
-  // if no connection skip rest
-
-  // maybe should retry 5 times?? see how we go we can probs afford to miss out on server data maybe ask Alfred
-
-  if(Serial.find("OK")){
+  if(Connected){
+    Serial.println(F("AT+CIPSTART=\"TCP\", \"192.168.100.100\", 81"));
+    if(Serial.find("OK")){
       // Send AT+CIPSEND=3,55 to say the serial content will have X length I think where x in the example is 55
 
       // may need to set to length of url
       send_next(F("AT+CIPSEND=55"));
-    
-      // Send GET /"path" HTTP:1/1 to prep request
 
       // finally send the data
 
@@ -171,19 +161,17 @@ void WIFI_send(String Time, bool PIR, String Temp, int CO2, String Dust){
         send_next(F("Connection: close"));
       }
       
-      // Send AT+CIPSEND=3,23 last number is the length of the data so length of path??
       send_next(F("AT+CIPSEND=23"));
-      
-    
-      // Send AT+CIPCLOSE=3 to end the tcp connection
 
       send_next(F("AT+CIPCLOSE"));
 
-    
-      // if we encounter ERROR we should probably cancel what we are doing somehow
-    
-      // may need to create my own find function to get the above working so maybe leave out for now
+    }
   }
+  
+
+  // if no connection skip rest
+
+  // maybe should retry 5 times?? see how we go we can probs afford to miss out on server data maybe ask Alfred
 
 }
 
--- Main/Wifi.ino
+++ Main/Wifi.ino
@@ -1,222 +1,222 @@
-///*
-// WiFiEsp example: WebClientRepeating
-//
-// This sketch connects to a web server and makes an HTTP request
-// using an Arduino ESP8266 module.
-// It repeats the HTTP call each 10 seconds.
-//
-// For more details see: http://yaab-arduino.blogspot.com/p/wifiesp.html
-//*/
-//
+/////*
+//// WiFiEsp example: WebClientRepeating
+////
+//// This sketch connects to a web server and makes an HTTP request
+//// using an Arduino ESP8266 module.
+//// It repeats the HTTP call each 10 seconds.
+////
+//// For more details see: http://yaab-arduino.blogspot.com/p/wifiesp.html
+////*/
+////
 ////#include "src/WiFiEsp.h"
 //
 ////char ssid[] = "4G UFI_8B8";           // your network SSID (name)
 ////char pass[] = "1234567890";        // your network password
 //
-////char ssid[] = "DESKTOP-73HN8ON 5011";           // your network SSID (name)
-////char pass[] = "u65879Q0"; 
-//
-//
-//
-//
+//////char ssid[] = "DESKTOP-73HN8ON 5011";           // your network SSID (name)
+//////char pass[] = "u65879Q0"; 
+////
+////
+////
+////
 ////int status = WL_IDLE_STATUS;     // the Wifi radio's status
-//
+////
 ////char server[] = "192.168.100.100";
-////char server[] = "seat-skomobo.massey.ac.nz";
-//
-//// sd card not present causes wifi connection to fail
-//
-//
-//
-////unsigned long lastConnectionTime = 0;         // last time you connected to the server, in milliseconds
-////const unsigned long postingInterval = 10000L; // delay between updates, in milliseconds
-//
-//// Initialize the Ethernet client object
+//////char server[] = "seat-skomobo.massey.ac.nz";
+////
+////// sd card not present causes wifi connection to fail
+////
+////
+////
+//////unsigned long lastConnectionTime = 0;         // last time you connected to the server, in milliseconds
+//////const unsigned long postingInterval = 10000L; // delay between updates, in milliseconds
+////
+////// Initialize the Ethernet client object
 ////WiFiEspClient client;
-//
+////
 ////void Wifi_setup()
 ////{
-//////  Serial2.begin(115200);
-////
-////  // initialize ESP module
-//////  WiFi.init(&Serial2);
+////////  Serial2.begin(115200);
+//////
+//////  // initialize ESP module
+////////  WiFi.init(&Serial2);
 ////  WiFi.init(&Serial);
+//////
+//////  // check for the presence of the shield
+//////  if (WiFi.status() == WL_NO_SHIELD) {
+//////
+//////      // do nothing for now eventually print error to display screen
+//////      
+//////    
+////////    Serial.println("WiFi shield not present");
+//////    
+//////    //change this so that it doesnt waste time doing other wifi setup stuff but does carry on to use sd card etc
+//////
+//////    // actually a hardware reset button to restart this code with a interrupt would be good, wire it in to pin 3???
+//////     
+//////    // don't continue
+////////    while (true);
+////// 
+//////    // if we cut out gfx then we may be able to use display
+//////
+//////      
+//////  }
 ////
-////  // check for the presence of the shield
-////  if (WiFi.status() == WL_NO_SHIELD) {
+////  // make sure that if any of these steps fail the system continues
 ////
-////      // do nothing for now eventually print error to display screen
-////      
+////  // attempt to connect to WiFi network
+//////  while ( status != WL_CONNECTED) {
+////////    Serial.print("Attempting to connect to WPA SSID: ");
+////////    Serial.println(ssid);
+//////    // Connect to WPA/WPA2 network
+//////    status = WiFi.begin(ssid, pass);
+////////  }
+//////  
+//////  for (int i = 0; i< 5; i= i + 1) {
+//////    if(status != WL_CONNECTED){
+//////       status = WiFi.begin(ssid, pass);
+//////    }
+//////    else{
+//////      break;
+//////    }
+//////  }
+////
+////  
+////
+////
+////  
+//////  Serial.println("You're connected to the network");
+////  
+//////  printWifiStatus();
+//////}
+//////
+//////void loop()
+//////{
+//////  // if there's incoming data from the net connection send it out the serial port
+//////  // this is for debugging purposes only
+////////  while (client.available()) {
+////////    char c = client.read();
+////////    Serial.write(c);
+////////  }
+//////
+//////  // if 10 seconds have passed since your last connection,
+//////  // then connect again and send data
+////////  if (millis() - lastConnectionTime > postingInterval) {
+////////    httpRequest();
+////////  }
+//////}
+////
+////// this method makes a HTTP connection to the server
+////void Wifi_send(String Time, bool PIR, String Temp, int CO2, String Dust)
+////{
+////  ////Serial.println();
 ////    
-//////    Serial.println("WiFi shield not present");
-////    
-////    //change this so that it doesnt waste time doing other wifi setup stuff but does carry on to use sd card etc
+////  // close any connection before send a new request
+////  // this will free the socket on the WiFi shield
 ////
-////    // actually a hardware reset button to restart this code with a interrupt would be good, wire it in to pin 3???
-////     
-////    // don't continue
-//////    while (true);
-//// 
-////    // if we cut out gfx then we may be able to use display
 ////
-////      
-////  }
-//
-//  // make sure that if any of these steps fail the system continues
-//
-//  // attempt to connect to WiFi network
-////  while ( status != WL_CONNECTED) {
-//////    Serial.print("Attempting to connect to WPA SSID: ");
-//////    Serial.println(ssid);
-////    // Connect to WPA/WPA2 network
+////
+////   // if status is not connected to hotspot then try and connect again in each loop
+////   // only attempt server connection if hotspot connection successful
+////   // only attempt get request if server connection successful 
+////   
+////
+////
+////  delay(10000);
+//////  Serial.println("Sending data");
+//////  client.stop(); 
+////  
+////  // invert this for cleanliness
+////
+////  // if no connection then try again
+////  
+////  if(status != WL_CONNECTED){
 ////    status = WiFi.begin(ssid, pass);
-//////  }
-////  
-////  for (int i = 0; i< 5; i= i + 1) {
-////    if(status != WL_CONNECTED){
-////       status = WiFi.begin(ssid, pass);
-////    }
-////    else{
-////      break;
-////    }
 ////  }
-//
-//  
-//
-//
-//  
-////  Serial.println("You're connected to the network");
-//  
-////  printWifiStatus();
-////}
-////
-////void loop()
-////{
-////  // if there's incoming data from the net connection send it out the serial port
-////  // this is for debugging purposes only
-//////  while (client.available()) {
-//////    char c = client.read();
-//////    Serial.write(c);
-//////  }
-////
-////  // if 10 seconds have passed since your last connection,
-////  // then connect again and send data
-//////  if (millis() - lastConnectionTime > postingInterval) {
-//////    httpRequest();
-//////  }
-////}
-//
-//// this method makes a HTTP connection to the server
-//void Wifi_send(String Time, bool PIR, String Temp, int CO2, String Dust)
-//{
-//  ////Serial.println();
-//    
-//  // close any connection before send a new request
-//  // this will free the socket on the WiFi shield
-//
-//
-//
-//   // if status is not connected to hotspot then try and connect again in each loop
-//   // only attempt server connection if hotspot connection successful
-//   // only attempt get request if server connection successful 
-//   
-//
-//
-//  delay(10000);
-////  Serial.println("Sending data");
-////  client.stop(); 
-//  
-//  // invert this for cleanliness
-//
-//  // if no connection then try again
-//  
-//  if(status != WL_CONNECTED){
-//    status = WiFi.begin(ssid, pass);
-//  }
-//  else{
-//     // if there's a successful connection
-//    if (client.connect(server, 81)) {
-//  
-//  //    Serial.println("Connecting...");
-//  
-//      // upgrade to SSL later should just be change from client.connect to client.connectSSL(ip etc)
-//      
-//      // send the HTTP PUT request
-//  
-//      //replace this with json
-//  //    Serial.println(Time[5]);
-//  //    client.println("GET /" BOX_ID "_" + Time[5] + "_" + Time[4] + "_" + Time[3] + "_" + Time[0] + "_" + Time[1] + "_" + Time[2] + "_" + Dust[0] + "_" + Dust[1] + "_" + Dust[2] + "_" + Temp[0] + "_" + Temp[1] + "_" + CO2 + "_" + PIR + " HTTP/1.1 ");
-//  
-//      // this will print the exact same line as client.println but it avoids the nasty issue where
-//      // the library appends shit unnesearrily due to this being a normal sring rather than a flash string
-//  
-//  //    Serial.println("GET /" BOX_ID "_" + Time + F("_") + Dust.replace(",", "_") + F("_") + Temp.replace(",", "_") + F("_") + CO2 + F("_") + PIR + F(" HTTP/1.1"));
-//      
-//      // look at the other message it sends first and also send that message think it is AT+CIPSEND ..... followed by AT+CIPCLOSE maybe
-//      
-////      Dust.replace(",", "_");
-////      Temp.replace(",", "_");
-////      Time.replace(":", "_");
-////      Time.replace("/", "_");
-////      Time.replace(" ", "_");
-//
-//
-//      // can the fucking wifi esp library and do it myself
-//
-//      
-//      // use normal serial because the library stuffs it up
-////      Serial.println("GET /" BOX_ID "_" + Time + F("_") + Dust + F("_") + Temp + F("_") + String(CO2) + F("_") + String(PIR) + F(" HTTP/1.1"));
-////      Serial.println(F("AT+CIPSEND=3,50"));
-////      Serial.println(F("AT+CIPCLOSE=3"));
-////      Serial.println("AT+CIPSEND 
-//     
-////      client.println(F("GET /0_2016_6_23_12332_12_31_23434_12_2434_1 HTTP:1/1"));
-//  //    client.println(F("Host: 192.168.100.100"));
-//      
-//      // change lib so that we can use .print properly so we can dynamically change the server etc
-//  //    client.println(F("Host: seat-skomobo.massey.ac.nz"));
-////      client.println(F("Host: 192.168.100.100"));
+////  else{
+////     // if there's a successful connection
+////    if (client.connect(server, 81)) {
 ////  
-////      client.println(F("Connection: close"));
-////      client.println();
-//  
-//  //    Serial.println("Data sent");
-//  
-//    // if there's incoming data from the net connection send it out the serial port
-//    // this is for debugging purposes only
-//  //    while (client.available()) {
-//  //      char c = client.read();
-//  //      Serial.write(c);
-//  //    }
-//  
-//  
-//      // note the time that the connection was made
-//  //    lastConnectionTime = millis();
-//    }
-//  //  else {
-//      // if you couldn't make a connection
-//  //    Serial.println("Connection failed");
-//  //  }
-//  }
-//}
-//
-//
-////void printWifiStatus()
-////{
-////  // print the SSID of the network you're attached to
-////  Serial.print("SSID: ");
-////  Serial.println(WiFi.SSID());
+////  //    Serial.println("Connecting...");
+////  
+////      // upgrade to SSL later should just be change from client.connect to client.connectSSL(ip etc)
+////      
+////      // send the HTTP PUT request
+////  
+////      //replace this with json
+////  //    Serial.println(Time[5]);
+////  //    client.println("GET /" BOX_ID "_" + Time[5] + "_" + Time[4] + "_" + Time[3] + "_" + Time[0] + "_" + Time[1] + "_" + Time[2] + "_" + Dust[0] + "_" + Dust[1] + "_" + Dust[2] + "_" + Temp[0] + "_" + Temp[1] + "_" + CO2 + "_" + PIR + " HTTP/1.1 ");
+////  
+////      // this will print the exact same line as client.println but it avoids the nasty issue where
+////      // the library appends shit unnesearrily due to this being a normal sring rather than a flash string
+////  
+////  //    Serial.println("GET /" BOX_ID "_" + Time + F("_") + Dust.replace(",", "_") + F("_") + Temp.replace(",", "_") + F("_") + CO2 + F("_") + PIR + F(" HTTP/1.1"));
+////      
+////      // look at the other message it sends first and also send that message think it is AT+CIPSEND ..... followed by AT+CIPCLOSE maybe
+////      
+//////      Dust.replace(",", "_");
+//////      Temp.replace(",", "_");
+//////      Time.replace(":", "_");
+//////      Time.replace("/", "_");
+//////      Time.replace(" ", "_");
 ////
-////  // print your WiFi shield's IP address
-////  IPAddress ip = WiFi.localIP();
-////  Serial.print("IP Address: ");
-////  Serial.println(ip);
 ////
-////  // print the received signal strength
-////  long rssi = WiFi.RSSI();
-////  Serial.print("Signal strength (RSSI):");
-////  Serial.print(rssi);
-////  Serial.println(" dBm");
+////      // can the fucking wifi esp library and do it myself
+////
+////      
+////      // use normal serial because the library stuffs it up
+//////      Serial.println("GET /" BOX_ID "_" + Time + F("_") + Dust + F("_") + Temp + F("_") + String(CO2) + F("_") + String(PIR) + F(" HTTP/1.1"));
+//////      Serial.println(F("AT+CIPSEND=3,50"));
+//////      Serial.println(F("AT+CIPCLOSE=3"));
+//////      Serial.println("AT+CIPSEND 
+////     
+//////      client.println(F("GET /0_2016_6_23_12332_12_31_23434_12_2434_1 HTTP:1/1"));
+////  //    client.println(F("Host: 192.168.100.100"));
+////      
+////      // change lib so that we can use .print properly so we can dynamically change the server etc
+////  //    client.println(F("Host: seat-skomobo.massey.ac.nz"));
+//////      client.println(F("Host: 192.168.100.100"));
+//////  
+//////      client.println(F("Connection: close"));
+//////      client.println();
+////  
+////  //    Serial.println("Data sent");
+////  
+////    // if there's incoming data from the net connection send it out the serial port
+////    // this is for debugging purposes only
+////  //    while (client.available()) {
+////  //      char c = client.read();
+////  //      Serial.write(c);
+////  //    }
+////  
+////  
+////      // note the time that the connection was made
+////  //    lastConnectionTime = millis();
+////    }
+////  //  else {
+////      // if you couldn't make a connection
+////  //    Serial.println("Connection failed");
+////  //  }
+////  }
 ////}
 ////
+////
+//////void printWifiStatus()
+//////{
+//////  // print the SSID of the network you're attached to
+//////  Serial.print("SSID: ");
+//////  Serial.println(WiFi.SSID());
+//////
+//////  // print your WiFi shield's IP address
+//////  IPAddress ip = WiFi.localIP();
+//////  Serial.print("IP Address: ");
+//////  Serial.println(ip);
+//////
+//////  // print the received signal strength
+//////  long rssi = WiFi.RSSI();
+//////  Serial.print("Signal strength (RSSI):");
+//////  Serial.print(rssi);
+//////  Serial.println(" dBm");
+//////}
+//////
 
