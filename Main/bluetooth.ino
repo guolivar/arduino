1e19b263e6eb92c8648a38251e9d621b9c6d0fb6
Ulfasaar <weyers.ryan@gmail.com>
2017-08-10 10:06:58 +1200

    Changed bluetooth transmission formatting to match formatting of data stored on SD card

--- Main/bluetooth.ino
+++ Main/bluetooth.ino
@@ -1,6 +1,6 @@
 void blue_send(){
 
     // improve this to get rid of buffer by printing each char one by one
-    snprintf_P(Buffer, 62, PSTR( BOX_ID "_%d-%d-%d-%d-%d-%d_%d_%d_%d_%d.%d_%d.%d_%d_%c"), year, month, day, hour, minute, second, PM1, PM25, PM10,(int)temperature, (int)(temperature * 100) % 100, (int)humidity, (int)(humidity * 100) % 100, CO2, PIR);
-    Serial.println(Buffer);
+    snprintf_P(Buffer, 60, PSTR("%d:%d:%d %d/%d/%d,%c,%d.%d,%d.%d,%d,%d,%d,%d\n"), hour, minute, second, day, month, year, PIR, (int)temperature, (int)(temperature * 100) % 100, (int)humidity, (int)(humidity * 100) % 100, CO2, PM1, PM25, PM10);
+    Serial.print(Buffer);
 }
